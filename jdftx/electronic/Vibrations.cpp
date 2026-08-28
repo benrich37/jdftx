5fafc59e5bfaf5117dd86f57464a25f19416ccec
benrich37 <benjamin.rich@colorado.edu>
2026-08-28 15:10:15 -0600

    removing type indicator when setting iConfigStart/Stop and computeOnly to hopefully access the private variable and not a new variable in the local scope

--- jdftx/electronic/Vibrations.cpp
+++ jdftx/electronic/Vibrations.cpp
@@ -80,10 +80,10 @@ void Vibrations::calculate()
 	logPrintf("iConfiguration = %d, nConfigurations = %d\n", iConfiguration, nConfigurations);
 	bool startSet = (iConfiguration>=0);
 	bool lenSet = (nConfigurations>=0);
-	int iConfigStart = startSet ? iConfiguration : 0;
-	int iConfigStop  = startSet ? (lenSet ? iConfiguration + nConfigurations : iConfiguration+1) : data.nModes;
+	iConfigStart = startSet ? iConfiguration : 0;
+	iConfigStop  = startSet ? (lenSet ? iConfiguration + nConfigurations : iConfiguration+1) : data.nModes;
 	logPrintf("iConfigStart = %d, iConfigStop = %d\n", iConfigStart, iConfigStop);
-	bool computeOnly = startSet or lenSet; //If either of these are set, do not bother in anything besides computing and writing grad and Pel
+	computeOnly = startSet or lenSet; //If either of these are set, do not bother in anything besides computing and writing grad and Pel
 	logPrintf("computeOnly = %d\n", computeOnly);
 	
 	//Determine number of degrees of freedom:
